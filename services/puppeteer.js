5d91a1c173cc22751bc46f3316bbd5987dd2093e
Jeff Jassky <jeff@jeffjassky.com>
2022-12-14 18:37:26 -0500

    Added dockerfile for cloud deployment

--- services/puppeteer.js
+++ services/puppeteer.js
@@ -1,18 +1,25 @@
 const puppeteer = require('puppeteer');
 let browser = null;
 let page = null;
+
+const settings = {
+    headless: true,
+    args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox'
+    ]
+}
+
+if(process.env.NODE_ENV === 'PRODUCTION'){
+    console.log('Using production settings');
+    console.log('Chroium path: /usr/bin/chromium-browser');
+    settings.executablePath = '/usr/bin/chromium-browser';
+}
+
 module.exports = {
     getBrowser: async function(){
         if(!browser){
-            browser = await puppeteer.launch(
-                {
-                    headless: true,
-                    args: [
-                        '--no-sandbox',
-                        '--disable-setuid-sandbox'
-                    ]
-                }
-            );
+            browser = await puppeteer.launch(settings);
         }
         return browser;
     },
